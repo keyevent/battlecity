cf3f99f6431c7a4f9702c355567c58655ce16d36
caoyu <caoyupersonal@hotmail.com>
2019-10-16 00:57:23 +0800

    radar turn independent

--- src/main/java/caoyu/Oreo.java
+++ src/main/java/caoyu/Oreo.java
@@ -1,27 +1,24 @@
 package caoyu;
 
 import robocode.*;
+import robocode.util.Utils;
 
 import java.awt.*;
 
 import static robocode.util.Utils.normalRelativeAngleDegrees;
 
 public class Oreo extends AdvancedRobot {
-    int robotHeading = 1;
-    int turnHeading = 1;
-    int move = 10;
-    int turnMove = 10;
-    int whiteCaneLength = 70;
+    int robotHeading = 1, turnHeading = 1;
+    int move = 10, turnMove = 10;
+    int whiteCaneLength = 100;
+    int diameter = 5;//行进直径
+    int counter = 0, denominator = 300;//自行转向的周期
 
-
-    double diameter = 5;
-
-    //counter
-    int counter = 0;
-    int denominator = 300;
+    Double lastScannedAbusoluteBearing;
 
     @Override
     public void run() {
+
         // Set colors
         setBodyColor(Color.black);
         setGunColor(Color.white);
@@ -29,11 +26,12 @@ public class Oreo extends AdvancedRobot {
         setScanColor(Color.white);
         setBulletColor(Color.white);
 
-        setAdjustRadarForGunTurn(false);
-        setAdjustRadarForRobotTurn(false);
+        setAdjustRadarForGunTurn(true);
+        setAdjustRadarForRobotTurn(true);
+        setAdjustGunForRobotTurn(true);
 
         //white cane out of wall condition
-        Condition whiteCaneOutOfBattlefieldCondition = new Condition("whiteCaneOutOfBattlefieldCondition", 11) {
+        Condition whiteCaneOutOfBattlefieldCondition = new Condition("whiteCaneOutOfBattlefieldCondition", 10) {
             @Override
             public boolean test() {
                 double whiteCaneX =
@@ -45,41 +43,12 @@ public class Oreo extends AdvancedRobot {
         };
         addCustomEvent(whiteCaneOutOfBattlefieldCondition);
 
-
-        setTurnGunRight(Double.POSITIVE_INFINITY);
+        setTurnRadarRight(540);
         // Loop forever
         while (true) {
             move();
-            turnGunRight(turnMove); // Scans automatically
+            execute();
         }
-    }
-
-    @Override
-    public void onHitRobot(HitRobotEvent event) {
-        if (event.isMyFault()) {
-            setMaxTurnRate(Rules.MAX_TURN_RATE);
-            turnRight(45);
-            robotHeading *= -1;
-            turnHeading *= -1;
-            move();
-            super.onHitRobot(event);
-        }
-    }
-
-    @Override
-    public void onScannedRobot(ScannedRobotEvent event) {// Calculate exact location of the robot
-        double absoluteBearing = getHeading() + event.getBearing();
-        double bearingFromGun = normalRelativeAngleDegrees(absoluteBearing - getGunHeading());
-
-        if (Math.abs(bearingFromGun) <= 3) {
-            turnGunRight(bearingFromGun);
-            if (event.getDistance() < 300) {
-                setFire(400 / event.getDistance());
-            }
-        } else {
-            turnGunRight(bearingFromGun);
-        }
-        move();
     }
 
     void move() {
@@ -91,12 +60,54 @@ public class Oreo extends AdvancedRobot {
         }
     }
 
+    void track(ScannedRobotEvent event) {
+        double bearingRadian = event.getBearingRadians();
+        double absoluteBearing = bearingRadian + getHeadingRadians();
+        double RadarOffset;
+        RadarOffset = Utils.normalRelativeAngle(absoluteBearing - getRadarHeadingRadians());
+        setTurnRadarRightRadians(RadarOffset * 1.2);
+    }
+
+    @Override
+    public void onHitRobot(HitRobotEvent event) {
+        if (event.isMyFault()) {
+            setMaxTurnRate(Rules.MAX_TURN_RATE);
+            robotHeading *= -1;
+            turnHeading *= -1;
+            move();
+            super.onHitRobot(event);
+        }
+    }
+
     @Override
     public void onCustomEvent(CustomEvent event) {
         if ("whiteCaneOutOfBattlefieldCondition" == event.getCondition().getName()) {
             setMaxTurnRate(Rules.MAX_TURN_RATE);
             turnHeading *= -1;
             turnRight(10);
+            setTurnRadarRight(-5);
         }
+    }
+
+    @Override
+    public void onScannedRobot(ScannedRobotEvent event) {// Calculate exact location of the robot
+        double absoluteBearing = getHeading() + event.getBearing();
+        double bearingFromGun = normalRelativeAngleDegrees(absoluteBearing - getGunHeading());
+
+        track(event);
+        if (Math.abs(bearingFromGun) <= 3) {
+            setTurnGunRight(bearingFromGun);
+            if (event.getDistance() < 300) {
+                setFire(400 / event.getDistance());
+            }
+        } else {
+            setTurnGunRight(bearingFromGun);
+        }
+        move();
+    }
+
+    @Override
+    public void onRobotDeath(RobotDeathEvent event) {
+        setTurnRadarRight(540);
     }
 }
